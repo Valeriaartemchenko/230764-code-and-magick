ad7d6816821cc4f9334ea9fe2d9fcf82c6bd71a4
Valeria Artemchemko <valeria.artemchenko@gmail.com>
2017-08-26 17:41:35 +0200

    отредактированно через ЕС линь

--- js/stat.js
+++ js/stat.js
@@ -1,65 +1,81 @@
-const CLOUD_POSITION = {
+'use strict';
+
+var CLOUD_POSITION = {
   x: 100,
   y: 10
-}
-const CLOUD_WIDTH = 420;
-const CLOUD_HEIGHT = 270;
-const CLOUD_COLOR = '#ffffff';
-const CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-const CLOUD_SHADOW_OFFSET = 10;
+};
 
-const CLOUD_TEXT_FAMILY = 'PT Mono';
-const CLOUD_TEXT_COLOR = '#000000';
-const CLOUD_TEXT_SIZE = 16;
-const CLOUD_TEXT_PADDING = 20;
-const CLOUD_TEXT = ['Ура вы победили!', 'Список результатов:'];
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_COLOR = '#ffffff';
+var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var CLOUD_SHADOW_OFFSET = 10;
 
-const HISTOGRAM_COLUMN_WIDTH = 40;
-const HISTOGRAM_COLUMN_HEIGHT = 150;
-const HISTOGRAM_COLUMN_MARGIN = 50;
-const HISTOGRAM_COLUMN_COLOR = 'rgba(0, 0, 255, ';
-const HISTOGRAM_COLUMN_COLOR_SELF = 'rgba(255, 0, 0, 1)';
+var CLOUD_TEXT_FAMILY = 'PT Mono';
+var CLOUD_TEXT_COLOR = '#000000';
+var CLOUD_TEXT_SIZE = 16;
+var CLOUD_TEXT_PADDING = 15;
+var CLOUD_TEXT = ['Ура вы победили!', 'Список результатов:'];
 
-const SELF_NAME = 'Вы';
+var HISTOGRAM_COLUMN_WIDTH = 40;
+var HISTOGRAM_COLUMN_HEIGHT = 150;
+var HISTOGRAM_COLUMN_MARGIN = 50;
+var HISTOGRAM_MARGIN_LEFT = 70;
+var HISTOGRAM_COLUMN_COLOR = 'rgba(0, 0, 255, ';
+var HISTOGRAM_COLUMN_COLOR_SELF = 'rgba(255, 0, 0, 1)';
+var HISTOGRAM_TEXT_INTERVAL = 5;
 
-var getPlayerColour = function(name){
+var SELF_NAME = 'Вы';
+var FIELD_HEIGHT = 250;
+
+var getPlayerColour = function (name) {
   return name === SELF_NAME ? HISTOGRAM_COLUMN_COLOR_SELF : HISTOGRAM_COLUMN_COLOR + Math.random() + ')';
-}
+};
 
-var getMaxTime = function(times){
-  var maxTime = 0;
-  for ( var i = 0; i < times.length; i++){
+var getMaxTime = function (times) {
+  var maxTime = getMaxTime(times);
+  for (var i = 0; i < times.length; i++) {
     maxTime = Math.max(maxTime, times[i]);
   }
   return maxTime;
-}
+};
 
-window.renderStatistics = function(ctx, names, times) {
+var step = HISTOGRAM_COLUMN_HEIGHT / getMaxTime;
+
+window.renderStatistics = function (ctx, names, times) {
   drawCloud(ctx);
   drawText(ctx);
   drawHistogram(ctx, names, times);
-}
+};
 
-var drawCloud = function(ctx) {
+var drawCloud = function (ctx) {
   ctx.fillStyle = CLOUD_SHADOW_COLOR;
   ctx.fillRect(CLOUD_POSITION.x + CLOUD_SHADOW_OFFSET, CLOUD_POSITION.y + CLOUD_SHADOW_OFFSET, CLOUD_WIDTH, CLOUD_HEIGHT);
 
   ctx.fillStyle = CLOUD_COLOR;
   ctx.fillRect(CLOUD_POSITION.x, CLOUD_POSITION.y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
-var drawText = function(ctx) {
+var drawText = function (ctx) {
   ctx.font = CLOUD_TEXT_SIZE + 'px ' + CLOUD_TEXT_FAMILY;
   ctx.fillStyle = CLOUD_TEXT_COLOR;
 
-  for(var i = 0; i < CLOUD_TEXT.length; i++){
+  for (var i = 0; i < CLOUD_TEXT.length; i++) {
     ctx.fillText(CLOUD_TEXT[i], CLOUD_POSITION.x + CLOUD_TEXT_PADDING, CLOUD_POSITION.y + (CLOUD_TEXT_SIZE + CLOUD_TEXT_PADDING) * (i + 1));
   }
-}
+};
 
-var drawHistogram = function(ctx, names, times) {
-  for(var i = 0; i < times.length; i++){
+var drawHistogram = function (ctx, names, times) {
+  for (var i = 0; i < times.length; i++) {
+    var columnHeight = times[i] * step;
+    var histogramColumnX = HISTOGRAM_MARGIN_LEFT + (HISTOGRAM_COLUMN_MARGIN + HISTOGRAM_COLUMN_WIDTH) * (i + 1);
+    var histogramColumnY = FIELD_HEIGHT - HISTOGRAM_COLUMN_HEIGHT;
     ctx.fillStyle = getPlayerColour(names[i]);
-    ctx.fillRect(CLOUD_POSITION.x, CLOUD_POSITION.y, HISTOGRAM_COLUMN_WIDTH, HISTOGRAM_COLUMN_HEIGHT);
+    ctx.fillRect(histogramColumnX, histogramColumnY, HISTOGRAM_COLUMN_WIDTH, HISTOGRAM_COLUMN_HEIGHT);
+    ctx.fillStyle = CLOUD_TEXT_COLOR;
+    ctx.fillText(Math.floor(times[i]), histogramColumnX, histogramColumnY - HISTOGRAM_TEXT_INTERVAL);
+    ctx.fillText(names[i], histogramColumnX, FIELD_HEIGHT + HISTOGRAM_TEXT_INTERVAL + CLOUD_TEXT_SIZE);
+
   }
-}
+};
+
